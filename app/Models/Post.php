4c7cc1d55a0634aac56e7654ba35d6fffa8c3050
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2023-03-20 09:15:24 -0500

    Lesson 38

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,15 @@ class Post extends Model
 
     protected $guarded = [];
 
-    //protected $with = ['category', 'author'];
+    protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters) 
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search)=>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+    }
 
     public function category()
     {
